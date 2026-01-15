e8b38a24b60b468f36a0af5be11de3843254ccc6
karleandrosilva <contatoparakarle@gmail.com>
2026-01-15 15:28:32 -0300

    update aula 02 parte 3

--- demo/src/main/java/app/controller/CarroController.java
+++ demo/src/main/java/app/controller/CarroController.java
@@ -3,6 +3,8 @@ package app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +33,19 @@ public class CarroController {
 		}
 		
 	}
-
+	
+	// método para buscar um carro pelo id
+	@GetMapping("/findById/{id}")
+	public ResponseEntity<Carro> findById(@PathVariable int id) {
+		
+		try {
+			Carro carro = this.carroService.findById(id);
+			return new ResponseEntity<>(carro, HttpStatus.OK); 
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+		
+	}
 }
 
 // PAROU 20:43
--- demo/src/main/java/app/service/CarroService.java
+++ demo/src/main/java/app/service/CarroService.java
@@ -13,5 +13,22 @@ public class CarroService {
 		return ("Carro: " + carro.getNome() + " salvo com sucesso!");
 		
 	}
-
+	
+	// método para buscar um carro pelo id
+	// simula a busca de um carro retornando um objeto fixo se o id for 1
+	public Carro findById(int id) {
+		
+		if(id == 1) {
+			Carro carro = new Carro();
+			
+			carro.setModelo("SUV");
+			carro.setMarca("abc");
+			carro.setNome("nome");
+			carro.setAnoFabricacao(1998);
+			
+			return carro;
+		} else {
+			return null;
+		}
+	}
 }
