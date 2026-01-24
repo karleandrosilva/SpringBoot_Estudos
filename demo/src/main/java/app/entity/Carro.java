059663aa17994f04d8dd271ce04a89f3572af3b5
karleandrosilva <contatoparakarle@gmail.com>
2026-01-24 01:03:08 -0300

    update aula 03A parte 2 - fix: ajusta application.properties

--- demo/src/main/java/app/entity/Carro.java
+++ demo/src/main/java/app/entity/Carro.java
@@ -1,7 +1,15 @@
 package app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // 1 - Declara que será uma entidade e que será mapeada para uma tabela no Banco de dados 
 public class Carro {
 	
+	@Id // Para definir que é um id, e marca como uma chave primaria da tabela
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Estratégia para gerar um identificador único no banco de dados de forma automática, permitindo que o Hibernate escolha a melhor forma de incremento (como sequências ou tabelas) de acordo com o banco utilizado.
 	private Long id;
 	private String nome;
 	private String marca;
