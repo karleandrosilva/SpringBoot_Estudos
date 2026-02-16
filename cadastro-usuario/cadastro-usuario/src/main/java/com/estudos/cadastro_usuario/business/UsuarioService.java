88e639894a53accb903b1f2460e908bf85c9a7b2
karleandrosilva <contatoparakarle@gmail.com>
2026-02-16 14:39:33 -0300

    update projeto CADASTRO_USUARIO parte 2 (Service e Repository)

--- cadastro-usuario/cadastro-usuario/src/main/java/com/estudos/cadastro_usuario/business/UsuarioService.java
+++ cadastro-usuario/cadastro-usuario/src/main/java/com/estudos/cadastro_usuario/business/UsuarioService.java
@@ -1,11 +1,38 @@
 package com.estudos.cadastro_usuario.business;
 
+import com.estudos.cadastro_usuario.infrastructure.entitys.Usuario;
+import com.estudos.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service // indica que é a classe de serviço
 public class UsuarioService {
 
     // para usar o métodos tem que usar o repository (injetar a dependencia do repository dentro do service)
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+    // método de salvar
+    public void salvarUsuario(Usuario usuario){
+        usuarioRepository.saveAndFlush(usuario); // salva e fecha a conexão com o bd
+    }
+
+    // método de buscar usuarios por email
+
+    // (se quer buscar por cpf é só colocar findByCpf | idade  = findByIdade)
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        // adiciona o findyByEmail no Repository, pois não existe no JPA Repository
+        return usuarioRepository.findByEmail(email).orElseThrow( // tratar o optional para retornar uma excessao personalizada
+                () -> new RuntimeException("Email não encontrado") // sera executado se não existir o (EMAIL)
+        );
+    }
+
+    // deletar usuario por email
+    public void deletarUsuarioPorEmail(String email) {
+        usuarioRepository.deletebyEmail(email);
+    }
+
+    // método update
 
 }
--- cadastro-usuario/cadastro-usuario/src/main/java/com/estudos/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ cadastro-usuario/cadastro-usuario/src/main/java/com/estudos/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,10 +1,19 @@
 package com.estudos.cadastro_usuario.infrastructure.repository;
 
 import com.estudos.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+// extende o JPA e o nome da tabela que se referncia (usuario
+// o tipo do campo id = INTEGER | mas pode ser LONG
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
-    // extende o JPA e o nome da tabela que se referncia (usuario
-    // o tipo do campo id = INTEGER | mas pode ser LONG
+    // adiciona o findyByEmail pois não existe
+    Optional<Usuario> findByEmail(String email); // optional = para criar uma excessao/alternativa, caso não exista o (EMAIL) procurado
+
+    // para deletar com email
+    @Transactional // caso de algum erro, ele não execulta (não deleta)
+    void deletebyEmail(String email);
 }
