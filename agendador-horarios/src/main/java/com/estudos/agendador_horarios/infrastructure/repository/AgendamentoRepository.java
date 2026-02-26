46efad157f0957f71c0af6b878e70f39b552834a
karleandrosilva <contatoparakarle@gmail.com>
2026-02-26 10:40:26 -0300

    update projeto AGENDADOR-HORARIOS parte 2.6/5 (Repository)

--- agendador-horarios/src/main/java/com/estudos/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
+++ agendador-horarios/src/main/java/com/estudos/agendador_horarios/infrastructure/repository/AgendamentoRepository.java
@@ -3,5 +3,10 @@ package com.estudos.agendador_horarios.infrastructure.repository;
 import com.estudos.agendador_horarios.infrastructure.entity.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> { //colocar interface e extends Jpa<Entidade,Long>
+
+    // between = entre os horarios
+    Agendamento findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFinal);
 }
--- agendador-horarios/src/main/java/com/estudos/agendador_horarios/service/AgendamentoService.java
+++ agendador-horarios/src/main/java/com/estudos/agendador_horarios/service/AgendamentoService.java
@@ -1,14 +1,31 @@
 package com.estudos.agendador_horarios.service;
 
+import com.estudos.agendador_horarios.infrastructure.entity.Agendamento;
 import com.estudos.agendador_horarios.infrastructure.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service // para mostrar que é uma service
 @RequiredArgsConstructor // injeção de dependencia |
 public class AgendamentoService {
 
-    private final AgendamentoRepository agendamentoRepository;
+    private final AgendamentoRepository agendamentoRepository; // chama injentando a dependencia
+
+    // salvar agendamento (verificar primeiro se a data e hora já está reservada)
+    public Agendamento salvarAgendamento(Agendamento agendamento){
+
+        // só consiga agendar uma pessoa a cada hora | gap de 1h
+        // não permite que ninguem seja agendado em horarios conflitantes
+        LocalDateTime horaAgendamento = agendamento.getDataHoraAgendamento();
+        LocalDateTime horaFim = agendamento.getDataHoraAgendamento().plusHours(1); // verificar se dentro do gap de 1h nao tem ninguem agendado
+
+
+        Agendamento agendados = agendamentoRepository.findByServicoAndDataHoraAgendamentoBetween(agendamento.getServico(), horaAgendamento,horaFim);
+
+        //21:47
+    }
 
 
 }
