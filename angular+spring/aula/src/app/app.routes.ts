6265ea73931e7c3445eb6ebf456bbf689cf8d3e2
karleandrosilva <contatoparakarle@gmail.com>
2026-04-23 23:54:57 -0300

    update aula #09 PARTE 3

--- angular+spring/aula/src/app/app.routes.ts
+++ angular+spring/aula/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Carrolist } from './components/carrolist/carrolist';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
     // Quando o usuário acessar a rota raiz (""), será redirecionado automaticamente para "/carros".
     // O pathMatch: 'full' garante que isso só aconteça quando o caminho estiver completamente vazio.
     {path: "", redirectTo : "carros", pathMatch : 'full'},
-    {path: "carros", component : Carrolist}
+    {path: "carros", component : Carrolist},
+     {path: "dashboard", component : Dashboard}
 ];
