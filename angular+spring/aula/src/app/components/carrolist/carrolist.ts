2e6d3268b4b1fe45b0c9aab379fcca5e1ebe0a2b
karleandrosilva <contatoparakarle@gmail.com>
2026-04-24 22:51:57 -0300

    update aula 10 parte 1/2

--- angular+spring/aula/src/app/components/carrolist/carrolist.ts
+++ angular+spring/aula/src/app/components/carrolist/carrolist.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Carro } from '../../models/carro';
 
 @Component({
   selector: 'app-carrolist',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './carrolist.scss',
 })
 export class Carrolist {
+
+  lista : Carro[] = [];
+
+  constructor(){
+    let carro : Carro = new Carro();
+    carro.id = 1;
+    carro.nome = "Fiesta";
+    carro.marca = "Ford";
+
+    let carro2 : Carro = new Carro();
+    carro.id = 2;
+    carro.nome = "Uno";
+    carro.marca = "Fiat";
+
+    let carro3 : Carro = new Carro();
+    carro.id = 3;
+    carro.nome = "HB20";
+    carro.marca = "Hydain";
+  }
 
 }
---
+++ angular+spring/aula/src/app/models/carro.ts
@@ -0,0 +1,7 @@
+export class Carro {
+
+    // atributos que tem no back
+    id! : number;
+    nome! : string;
+    marca! : string;
+}
