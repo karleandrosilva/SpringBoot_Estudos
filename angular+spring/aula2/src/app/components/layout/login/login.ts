94540430549df3fa20d7a05d169c4b72d94fef9f
karleandrosilva <contatoparakarle@gmail.com>
2026-07-14 22:33:32 -0300

    update aula 12 part 0.7/3.6

--- angular+spring/aula2/src/app/components/layout/login/login.ts
+++ angular+spring/aula2/src/app/components/layout/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MdbFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
