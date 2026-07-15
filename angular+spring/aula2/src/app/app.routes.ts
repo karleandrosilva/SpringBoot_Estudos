a4fc370f7886d6087f8f29bc4fd8768912cdb771
karleandrosilva <contatoparakarle@gmail.com>
2026-07-14 21:21:02 -0300

    update aula 11 coments

--- angular+spring/aula2/src/app/app.routes.ts
+++ angular+spring/aula2/src/app/app.routes.ts
@@ -7,8 +7,8 @@ import { Carrosdetails } from './components/carros/carrosdetails/carrosdetails';
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch : 'full'},
     {path: "login", component: Login},
-    {path: "admin", component: Principal, children: [
-        {path: "carros", component: Carro},
+    {path: "admin", component: Principal, children: [ // Tem o <app-menu> e o <router-outlet> no HTML dele
+        {path: "carros", component: Carro}, // Renderiza dentro do outlet do Principal
         {path: "carros/new", component: Carrosdetails},
         {path: "carros/edit/:id", component: Carrosdetails}
     ]}
