fea5b6ed777857cfd67910285eb3419237edc353
karleandrosilva <contatoparakarle@gmail.com>
2026-07-20 22:16:23 -0300

    update aula 12 part 2/3.6

--- angular+spring/aula2/src/app/app.routes.ts
+++ angular+spring/aula2/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Login } from './components/layout/login/login';
 import { Principal } from './components/layout/principal/principal';
 import { Carro } from './models/carro';
 import { Carrosdetails } from './components/carros/carrosdetails/carrosdetails';
+import { Marca } from './models/marca';
+import { Marcasdetails } from './components/marcas/marcasdetails/marcasdetails';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch : 'full'},
@@ -10,6 +12,9 @@ export const routes: Routes = [
     {path: "admin", component: Principal, children: [ // Tem o <app-menu> e o <router-outlet> no HTML dele
         {path: "carros", component: Carro}, // Renderiza dentro do outlet do Principal
         {path: "carros/new", component: Carrosdetails},
-        {path: "carros/edit/:id", component: Carrosdetails}
+        {path: "carros/edit/:id", component: Carrosdetails},
+        {path: "marcas", component: Marca}, 
+        {path: "marcas/new", component: Marcasdetails},
+        {path: "marcas/edit/:id", component: Marcasdetails}
     ]}
 ];
