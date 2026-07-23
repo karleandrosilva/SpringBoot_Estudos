c3fa4706c7b7b2a1a4189b4aefb333bb56fd7d6b
karleandrosilva <contatoparakarle@gmail.com>
2026-07-22 22:46:42 -0300

    update aula 12 part 2.4/3.6

--- angular+spring/aula2/src/app/app.routes.ts
+++ angular+spring/aula2/src/app/app.routes.ts
@@ -5,15 +5,17 @@ import { Carro } from './models/carro';
 import { Carrosdetails } from './components/carros/carrosdetails/carrosdetails';
 import { Marca } from './models/marca';
 import { Marcasdetails } from './components/marcas/marcasdetails/marcasdetails';
+import { Carroslist } from './components/carros/carroslist/carroslist';
+import { Marcaslist } from './components/marcas/marcaslist/marcaslist';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch : 'full'},
     {path: "login", component: Login},
     {path: "admin", component: Principal, children: [ // Tem o <app-menu> e o <router-outlet> no HTML dele
-        {path: "carros", component: Carro}, // Renderiza dentro do outlet do Principal
+        {path: "carros", component: Carroslist}, // Renderiza dentro do outlet do Principal
         {path: "carros/new", component: Carrosdetails},
         {path: "carros/edit/:id", component: Carrosdetails},
-        {path: "marcas", component: Marca}, 
+        {path: "marcas", component: Marcaslist}, 
         {path: "marcas/new", component: Marcasdetails},
         {path: "marcas/edit/:id", component: Marcasdetails}
     ]}
--- angular+spring/aula2/src/app/components/carros/carroslist/carroslist.ts
+++ angular+spring/aula2/src/app/components/carros/carroslist/carroslist.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Carro } from '../../../models/carro';
 
 @Component({
   selector: 'app-carroslist',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './carroslist.scss',
 })
 export class Carroslist {
+
+  // criar uma lista 
+  lista : Carro[] = [];
+
+  //
+  constructor() {
+    this.lista.push(new Carro(1, 'Fiesta'));
+    this.lista.push(new Carro(2, 'Monza'));
+    this.lista.push(new Carro(3, 'Uno'));
+  }
 
 }
