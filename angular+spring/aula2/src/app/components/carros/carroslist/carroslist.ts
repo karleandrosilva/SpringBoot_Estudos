5a8a1bc8f5bddea54fcb3078b7b575895ecb9d2f
karleandrosilva <contatoparakarle@gmail.com>
2026-07-28 21:34:43 -0300

    update aula 12 part 2.8/3.6

--- angular+spring/aula2/src/app/components/carros/carroslist/carroslist.ts
+++ angular+spring/aula2/src/app/components/carros/carroslist/carroslist.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { Carro } from '../../../models/carro';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-carroslist',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './carroslist.html',
   styleUrl: './carroslist.scss',
 })
 export class Carroslist {
 
-  // criar uma lista 
+  // cria uma lista 
   lista : Carro[] = [];
 
-  //
+  // inicializa a lista com alguns carros já adicionados
   constructor() {
     this.lista.push(new Carro(1, 'Fiesta'));
     this.lista.push(new Carro(2, 'Monza'));
     this.lista.push(new Carro(3, 'Uno'));
+  }
+
+  editar() {
+
+  }
+
+  deletar() {
+
   }
 
 }
