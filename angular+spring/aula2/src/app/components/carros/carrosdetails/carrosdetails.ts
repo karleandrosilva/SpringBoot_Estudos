3354b8163299893e03b18924185565212277c426
karleandrosilva <contatoparakarle@gmail.com>
2026-08-02 20:25:39 -0300

    update aula 12 part final

--- angular+spring/aula2/src/app/components/carros/carrosdetails/carrosdetails.ts
+++ angular+spring/aula2/src/app/components/carros/carrosdetails/carrosdetails.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Carro } from '../../../models/carro';
 
 @Component({
   selector: 'app-carrosdetails',
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './carrosdetails.html',
   styleUrl: './carrosdetails.scss',
 })
 export class Carrosdetails {
+
+  carro : Carro = new Carro(0, "");
+
+  save() {
+    alert('Salvo com sucesso!')
+  }
 
 }
